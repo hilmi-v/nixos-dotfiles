7306627a4ef83982a61468bafe01fe1ca7f3ef69
hilmi-v <alfalahhilmi@gmail.com>
2026-09-20 17:12:41 +0700

    rebuild: system update 2026-09-20 17:11:45

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     #   url = "github:gmodena/nix-flatpak/?ref=latest";
     # };
     claude-code.url = "github:sadjow/claude-code-nix";
-    local-cashier.url = "git+file:///home/hilmi/projects/local_cashier";
+    # local-cashier.url = "git+file:///home/hilmi/projects/local_cashier";
   };
 
   outputs =
@@ -38,7 +38,7 @@
       spotx-nix,
       # nix-flatpak,
       claude-code,
-      local-cashier,
+      
       ...
     }@inputs:
     {
@@ -63,16 +63,7 @@
             };
             nixpkgs.overlays = [ claude-code.overlays.default ];
           }
-           local-cashier.nixosModules.default
-        {
-          services.local-cashier = {
-            enable = true;
-            appDir = "/home/hilmi/local_cashier";   # the checkout, already built
-            user = "hilmi";
-            # dataDir defaults to /var/lib/local-cashier
-            # port defaults to 3210
-          };
-        }
+       
 
           # secure boot
           # lanzaboote.nixosModules.lanzaboote
